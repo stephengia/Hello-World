3fa0f005687c8846dfbbc0d6fdac1b1edb71a93d
Stephen Tran <transtephen2402@gmail.com>
2022-04-18 15:14:25 -0400

    Third commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! My name is Gia Bao Tran!");
+        System.out.println("Toi la mot thanh weebu");
     }
 }
